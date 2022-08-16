272a4c6fe692e28bccd14be6413434b9d03bc0de
ankerx <93661548+ankerx@users.noreply.github.com>
2022-08-16 09:36:05 -0400

    fix deploy

--- graphql/apollo.ts
+++ graphql/apollo.ts
@@ -3,6 +3,6 @@ import { ApolloClient, InMemoryCache } from "@apollo/client";
 const graphqlAPI = process.env.GRAPH_CMS!;
 
 export const client = new ApolloClient({
-  uri: graphqlAPI,
+  uri: "https://api-eu-central-1.graphcms.com/v2/cl3k3iamy9a4801zdehnbhmqw/master",
   cache: new InMemoryCache(),
 });

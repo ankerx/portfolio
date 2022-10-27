aca782c8329280ba56d11e746c8ea9650f3b8b81
ankerx <93661548+ankerx@users.noreply.github.com>
2022-10-27 12:22:43 -0400

    fix: deploy on vercel

--- graphql/apollo.ts
+++ graphql/apollo.ts
@@ -1,6 +1,6 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: process.env.GRAPH_CMS,
+  uri: process.env.NEXT_PUBLIC_GRAPH_CMS,
   cache: new InMemoryCache(),
 });
